60cbf52e3ac23ffc439fb60fe1315155ac808834
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-02 21:22:11 +0200

    Better sample which show getter/setter usage in decompilation

--- hello-properties/Program.cs
+++ hello-properties/Program.cs
@@ -1,7 +1,22 @@
 ﻿var world = new World();
+world.ComplexName = "Universe";
 Console.WriteLine($"Hello, {world.Name}!");
+Console.WriteLine($"Hello, {world.ComplexName}!");
 
 class World
 {
     public string Name { get; set; } = "World";
+    public string ComplexName
+    { 
+        get
+        {
+            Console.WriteLine($"Getting ComplexName, which is {field}");
+            return field;
+        }
+        set
+        {
+            field = value;
+            Console.WriteLine($"Set ComplexName to {value}");   
+        }
+     } = "World";
 }
